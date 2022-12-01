728eb9525f960b84dd572a32065fbf460ce10608
SahilAnower <sahilanower2@gmail.com>
2022-12-01 22:35:12 +0530

    CORS added

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import orderRouter from './routes/orderRoutes.js';
-
+import cors from 'cors';
 dotenv.config();
 
 mongoose
@@ -19,6 +19,8 @@ mongoose
   });
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
