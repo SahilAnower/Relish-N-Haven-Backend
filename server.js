bf8b5ff30c61145cba9cf445a0b7d187fc4f3404
SahilAnower <sahilanower2@gmail.com>
2022-12-17 13:04:29 +0530

    Access Control

--- server.js
+++ server.js
@@ -20,6 +20,14 @@ mongoose
 
 const app = express();
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 app.use(cors());
 
 app.use(express.json());
